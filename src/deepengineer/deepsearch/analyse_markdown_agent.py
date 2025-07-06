41cc805b77480fcfe572e440119356c01b25f8d5
Charles Azam <azamcharles0@gmail.com>
2025-07-06 23:15:07 +0200

    feat: add test for crawl_database

--- src/deepengineer/deepsearch/analyse_markdown_agent.py
+++ src/deepengineer/deepsearch/analyse_markdown_agent.py
@@ -1,3 +1,7 @@
+"""
+Simple agent to analyse a markdown, just to test some ideas.
+"""
+
 from smolagents import CodeAgent, tool, Tool, LiteLLMModel
 from deepengineer.webcrawler.pdf_utils import get_markdown_by_page_numbers, get_table_of_contents_per_page_markdown, find_in_markdown, convert_ocr_response_to_markdown
 from mistralai import OCRResponse
--- tests/webcrawler/test_crawl_database.py
+++ tests/webcrawler/test_crawl_database.py
@@ -16,4 +16,9 @@ def test_crawl_database_arxiv_link():
     assert db.get_markdown_of_url("https://arxiv.org/abs/2105.00643").pages[0].markdown is not None
     assert len(db.get_markdown_of_url("https://arxiv.org/abs/2105.00643").pages) == 20
     
-    
+def test_crawl_database_wikipedia_url():
+    db = DataBase()
+    db.crawl_url("https://en.wikipedia.org/wiki/Deep_learning")
+    assert db.get_markdown_of_url("https://en.wikipedia.org/wiki/Deep_learning") is not None
+    assert db.get_markdown_of_url("https://en.wikipedia.org/wiki/Deep_learning").pages[0].markdown is not None
+    assert len(db.get_markdown_of_url("https://en.wikipedia.org/wiki/Deep_learning").pages) >= 40
