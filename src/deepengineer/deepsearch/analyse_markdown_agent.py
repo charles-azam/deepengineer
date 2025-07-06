5a35d9f764e8cb0befc5522c3697068ff9ae98cf
Charles Azam <azamcharles0@gmail.com>
2025-07-06 16:50:06 +0200

    feat: add pdf agent

--- src/deepengineer/deepsearch/analyse_markdown_agent.py
+++ src/deepengineer/deepsearch/analyse_markdown_agent.py
@@ -1,52 +1,98 @@
-def create_agent(model_id="o1"):
-    model_params = {
-        "model_id": model_id,
-        "custom_role_conversions": custom_role_conversions,
-        "max_completion_tokens": 8192,
-    }
-    if model_id == "o1":
-        model_params["reasoning_effort"] = "high"
-    model = LiteLLMModel(model_id="deepseek/deepseek-chat")
+from smolagents import CodeAgent, tool, Tool, LiteLLMModel
+from deepengineer.webcrawler.pdf_utils import get_markdown_by_page_numbers, get_table_of_contents_per_page_pdf, find_in_pdf, convert_ocr_response_to_markdown
+from mistralai import OCRResponse
+from enum import Enum
+from pathlib import Path
 
-    text_limit = 100000
-    browser = SimpleTextBrowser(**BROWSER_CONFIG)
-    WEB_TOOLS = [
-        GoogleSearchTool(provider="serper"),
-        VisitTool(browser),
-        PageUpTool(browser),
-        PageDownTool(browser),
-        FinderTool(browser),
-        FindNextTool(browser),
-        ArchiveSearchTool(browser),
-        TextInspectorTool(model, text_limit),
+class ToolNames(Enum):
+    GET_TABLE_OF_CONTENTS = "get_table_of_contents"
+    GET_MARKDOWN = "get_markdown"
+    GET_PAGES_CONTENT = "get_pages_content"
+    FIND_IN_PDF = "find_in_pdf"
+
+class GetTableOfContentsTool(Tool):
+    name = ToolNames.GET_TABLE_OF_CONTENTS.value
+    description = "Returns all of the titles in the document along with the page number they are on."
+    inputs = {}
+    output_type = "string"
+    
+    def __init__(self, markdown: OCRResponse):
+        self.markdown: OCRResponse = markdown
+        self.table_of_contents: str = get_table_of_contents_per_page_pdf(self.markdown)
+        
+    def forward(self) -> str:
+        return self.table_of_contents
+    
+class GetMarkdownTool(Tool):
+    name = ToolNames.GET_MARKDOWN.value
+    description = f"Returns the markdown entire content of the document. Beware this might be too long to be useful, except for small documents, use {ToolNames.GET_PAGES_CONTENT.value} instead. You can use {ToolNames.GET_TABLE_OF_CONTENTS.value} to get the table of contents of the document including the number of pages."
+    inputs = {}
+    output_type = "string"
+    
+    def __init__(self, markdown: OCRResponse):
+        self.markdown: OCRResponse = markdown
+        self.markdown_content: str = convert_ocr_response_to_markdown(self.markdown)
+        
+    def forward(self) -> str:
+        return self.markdown_content
+    
+    
+class GetPagesContentTool(Tool):
+    name = ToolNames.GET_PAGES_CONTENT.value
+    description = f"Returns the content of the pages. You can use {ToolNames.GET_TABLE_OF_CONTENTS.value} to get the table of contents of the document including the number of pages. Expects a list of page numbers as integers as input."
+    inputs = {
+        "page_numbers": {
+            "type": "array",
+            "description": "The page numbers to get the content of."
+        },
+    }
+    output_type = "string"
+    
+    def __init__(self, markdown: OCRResponse):
+        self.markdown: OCRResponse = markdown
+
+    def forward(self, page_numbers: list[int]) -> str:
+        return get_markdown_by_page_numbers(self.markdown, page_numbers)
+    
+class FindInPdfTool(Tool):
+    name = ToolNames.FIND_IN_PDF.value
+    description = f"Finds the page numbers of the document that contain the search queries. If you are looking for a specific information, you can use this tool to find the page numbers of the document that contain the information and then use {ToolNames.GET_PAGES_CONTENT.value} to get the content of the pages."
+    inputs = {
+        "search_queries": {
+            "type": "array",
+            "description": "The search queries to find in the document. List of strings."
+        }
+    }
+    output_type = "array"
+    
+    def __init__(self, markdown: OCRResponse):
+        self.markdown: OCRResponse = markdown
+    
+    def forward(self, search_queries: list[str]) -> list[int]:
+        return find_in_pdf(self.markdown, search_queries)
+
+
+        
+def create_agent(markdown: OCRResponse, model_id="deepseek/deepseek-chat"):
+
+    model = LiteLLMModel(model_id=model_id)
+
+    PDFS_TOOLS = [
+        GetTableOfContentsTool(markdown),
+        GetMarkdownTool(markdown),
+        GetPagesContentTool(markdown),
+        FindInPdfTool(markdown),
     ]
-    text_webbrowser_agent = ToolCallingAgent(
+    pdf_agent = CodeAgent(
         model=model,
-        tools=WEB_TOOLS,
+        tools=PDFS_TOOLS,
         max_steps=20,
         verbosity_level=2,
         planning_interval=4,
-        name="search_agent",
-        description="""A team member that will search the internet to answer your question.
-    Ask him for all your questions that require browsing the web.
-    Provide him as much context as possible, in particular if you need to search on a specific timeframe!
-    And don't hesitate to provide him with a complex search task, like finding a difference between two webpages.
-    Your request must be a real sentence, not a google search! Like "Find me this information (...)" rather than a few keywords.
-    """,
+        name="pdf_agent",
+        description="""A team member that will search the internet to answer your question.""",
         provide_run_summary=True,
     )
-    text_webbrowser_agent.prompt_templates["managed_agent"]["task"] += """You can navigate to .txt online files.
-    If a non-html page is in another format, especially .pdf or a Youtube video, use tool 'inspect_file_as_text' to inspect it.
-    Additionally, if after some searching you find out that you need more information to answer the question, you can use `final_answer` with your request for clarification as argument to request for more information."""
+    pdf_agent.prompt_templates["managed_agent"]["task"] += """You can navigate to .txt online files."""
 
-    manager_agent = CodeAgent(
-        model=model,
-        tools=[visualizer, TextInspectorTool(model, text_limit)],
-        max_steps=12,
-        verbosity_level=2,
-        additional_authorized_imports=["*"],
-        planning_interval=4,
-        managed_agents=[text_webbrowser_agent],
-    )
-
-    return manager_agent
+    return pdf_agent
