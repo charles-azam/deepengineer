395c3d4070c93633b1196c075cc27d792f1f3778
Charles Azam <azamcharles0@gmail.com>
2025-07-13 20:20:31 +0200

    feat: add draw image tool

--- tests/webcrawler/test_draw_agent.py
+++ tests/webcrawler/test_draw_agent.py
@@ -2,9 +2,11 @@ import pytest
 from deepengineer.deepsearch.draw_agent import (
     draw_image_agent,
     multiple_steps_draw_image_agent,
+    SaveMatplotlibFigTool,
 )
 from deepengineer.common_path import DATA_DIR
 from pathlib import Path
+from smolagents import CodeAgent, LiteLLMModel
 
 
 @pytest.mark.expensive
@@ -14,6 +16,32 @@ def test_draw_image_agent():
     output_path.unlink(missing_ok=True)
     output_path = draw_image_agent(prompt, output_path, multiple_steps=False)
     assert output_path.exists()
+
+
+def test_save_matplotlib_fig_tool():
+    model = LiteLLMModel(model_id="mistral/mistral-medium-latest")
+    agent = CodeAgent(
+        tools=[SaveMatplotlibFigTool(output_dir=Path(DATA_DIR))],
+        model=model,
+        additional_authorized_imports=[
+            "matplotlib.*",
+            "numpy.*",
+            "pandas.*",
+            "seaborn.*",
+        ],
+        max_steps=20,
+        verbosity_level=2,
+    )
+    agent.run(
+        """
+              Propose moi un schéma très détaillé d'un réacteur nucléaire hélium graphite.
+              Save the figure as figure.png. Return a markdown string where you explain what you did and then include the image.
+              """
+    )
+    assert (Path(DATA_DIR) / "figure.png").exists()
+
+
+test_save_matplotlib_fig_tool()
 
 
 @pytest.mark.skip(reason="This function is not working yet")
