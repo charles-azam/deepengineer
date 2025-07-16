485e053be703991c3390fcfec2ba46368ab39fd2
Charles Azam <azamcharles0@gmail.com>
2025-07-16 23:02:33 +0200

    feat: check if on spaces

--- src/deepengineer/common_path.py
+++ src/deepengineer/common_path.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+import os
+
+def is_on_spaces():
+    return "SPACE_ID" in os.environ or "HF_SPACE_ID" in os.environ
+
 
 DEEPENGINEER_CODE_DIR = Path(__file__).parent
 DEEPENGINEER_SRC_DIR = DEEPENGINEER_CODE_DIR.parent
@@ -7,5 +12,9 @@ DEEPENGINEER_ROOT_DIR = DEEPENGINEER_SRC_DIR.parent
 assert DEEPENGINEER_CODE_DIR.name == "deepengineer"
 assert DEEPENGINEER_SRC_DIR.name == "src"
 
-DATA_DIR = DEEPENGINEER_ROOT_DIR / "data"
+if is_on_spaces():
+    DATA_DIR = Path("/data")
+else:
+    DATA_DIR = DEEPENGINEER_ROOT_DIR / "data"
+
 assert DATA_DIR.exists()
